3b7da6a4dd21d016143927ab6433bd7245acdf19
kjkerr01 <kainoa1kerr@gmail.com>
2025-10-20 12:19:21 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Upgrade pip
+# Ensure pip is up-to-date
 pip install --upgrade pip
-# Install Playwright browsers (only Chromium)
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Install Playwright browsers (Chromium only)
 python -m playwright install chromium
